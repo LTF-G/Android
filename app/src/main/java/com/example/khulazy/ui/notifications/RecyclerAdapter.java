f135ba10c00ca856f594a2403a66ee77da4183b9
josushell <yoy07030@khu.ac.kr>
2022-11-30 15:58:43 +0900

    [Feat] 인증서 ssl 해결

--- app/src/main/java/com/example/khulazy/ui/notifications/RecyclerAdapter.java
+++ app/src/main/java/com/example/khulazy/ui/notifications/RecyclerAdapter.java
@@ -1,6 +1,7 @@
 package com.example.khulazy.ui.notifications;
 
 import android.content.Context;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.TextureView;
@@ -17,11 +18,21 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.khulazy.MainActivity;
 import com.example.khulazy.R;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.InetAddress;
+import java.net.MalformedURLException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.logging.Handler;
 
@@ -30,29 +41,68 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
     String response; //서버 응답
     String TAG = "socket 응답";
 //    Handler handler = new Handler();
+    onHTTPConnection httpConnection = new onHTTPConnection();
 
     class SocketThread extends Thread{
         String host; // 서버 IP
         String data; // 전송 데이터
+        String rpi; // 라즈베리 파이 ip
 
         public SocketThread(String host, String data){
             this.host = host;
             this.data = data;
+            this.rpi = "127.0.0.1";
         }
 
         @Override
         public void run() {
             try{
                 int port = 5555; //포트 번호는 서버측과 똑같이
-                Socket socket = new Socket(host, port); // 소켓 열어주기
+
+                int SDK_INT = android.os.Build.VERSION.SDK_INT;
+                if (SDK_INT > 8){
+                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                    StrictMode.setThreadPolicy(policy);
+                }
+
+                rpi = httpConnection.GETFunction(this.host);
+                Socket socket = new Socket(rpi, port); // 소켓 열어주기
+
                 ObjectOutputStream outstream = new ObjectOutputStream(socket.getOutputStream()); //소켓의 출력 스트림 참조
                 outstream.writeObject(data); // 출력 스트림에 데이터 넣기
                 outstream.flush(); // 출력
 
-                ObjectInputStream instream = new ObjectInputStream(socket.getInputStream()); // 소켓의 입력 스트림 참조
-                response = (String) instream.readObject(); // 응답 가져오기
-                Log.d(TAG, "response: " + response);
+                String OutData = "AT+START\n";
+                byte[] data = OutData.getBytes();
+                OutputStream output = socket.getOutputStream();
+                output.write(data);
 
+                Log.d(TAG, "데이터 보냄");
+
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        Toast.makeText(mContext.getApplicationContext(), "소켓 연결됨", Toast.LENGTH_LONG).show();
+//                    }
+//                });
+
+//                while (true) {
+//                    byte[] buffer = new byte[1024];
+//                    InputStream input = socket.getInputStream();
+//                    int bytes = input.read(buffer);
+//
+//                    Log.d(TAG, "byte = " + bytes);
+//                }
+
+                while(true) {
+                    ObjectInputStream instream = new ObjectInputStream(socket.getInputStream()); // 소켓의 입력 스트림 참조
+                    response = (String) instream.readObject(); // 응답 가져오기
+                    Log.d(TAG, "response: " + response);
+
+                    if (response == "stop") {
+                        break;
+                    }
+                }
 //                handler.post(new Runnable() {
 //                    @Override
 //                    public void run() {
@@ -91,12 +141,12 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
             public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
                 if (checked) {
                     Toast.makeText(mContext, "알람을 설정했습니다.", Toast.LENGTH_SHORT).show();
-                    SocketThread thread = new SocketThread("hostip", "data");
+                    SocketThread thread = new SocketThread("https://43.201.130.48:8484/connection", "set");
                     thread.start();
                 }
                 else {
                     Toast.makeText(mContext, "알람을 해제했습니다.", Toast.LENGTH_SHORT).show();
-                    SocketThread thread = new SocketThread("hostip", "data");
+                    SocketThread thread = new SocketThread("https://43.201.130.48:8484/connection", "cancel");
                     thread.start();
                 }
             }
@@ -127,3 +177,4 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         }
     }
 }
+
---
+++ app/src/main/java/com/example/khulazy/ui/notifications/onHTTPConnection.java
@@ -0,0 +1,108 @@
+package com.example.khulazy.ui.notifications;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+public class onHTTPConnection {
+
+    String EXCEPTION_ERROR = "url 오류 발생";
+
+    public String GETFunction(String mUrl) {
+        try {
+            URL url = new URL(mUrl);
+            ignoreSsl();
+            
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFzZCIsImlhdCI6MTY2OTQ5MDg5MCwiZXhwIjoxNjY5NDk4MDkwfQ.TjmeuMwEcnY7str3L2DyEt3QTD6ZSKTK5mBa5Eqxc7Q");
+            conn.setRequestProperty("refresh", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Njk0OTA4OTAsImV4cCI6MTY3MDcwMDQ5MH0.KTc1GuJZAyYB3lvtZZ4W8XLjnEZh97YL8bNcDSCoxZw");
+            conn.setConnectTimeout(15000);
+            conn.setReadTimeout(10000);
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            InputStream is = conn.getInputStream();
+            StringBuilder sb = new StringBuilder();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String result;
+            while ((result = br.readLine()) != null) {
+                sb.append(result);
+            }
+            br.close();
+
+            result = sb.toString();
+
+            JSONArray jsonResponse = new JSONArray(result);
+            JSONObject jsonObject = jsonResponse.getJSONObject(0);
+            result = jsonObject.getString("ip");
+
+            return result;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Log.d("ERROR", EXCEPTION_ERROR);
+        return null;
+    }
+
+    public static void ignoreSsl() throws Exception {
+        HostnameVerifier hv = new HostnameVerifier() {
+            public boolean verify(String urlHostName, SSLSession session) {
+                return true;
+            }
+        };
+        trustAllHttpsCertificates();
+        HttpsURLConnection.setDefaultHostnameVerifier(hv);
+    }
+
+    private static void trustAllHttpsCertificates() throws Exception {
+        TrustManager[] trustAllCerts = new TrustManager[1];
+        TrustManager tm = new miTM();
+        trustAllCerts[0] = tm;
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, null);
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+    }
+
+    static class miTM implements TrustManager, X509TrustManager {
+        public X509Certificate[] getAcceptedIssuers() {
+            return null;
+        }
+
+        public boolean isServerTrusted(X509Certificate[] certs) {
+            return true;
+        }
+
+        public boolean isClientTrusted(X509Certificate[] certs) {
+            return true;
+        }
+
+        public void checkServerTrusted(X509Certificate[] certs, String authType)
+                throws CertificateException {
+            return;
+        }
+
+        public void checkClientTrusted(X509Certificate[] certs, String authType)
+                throws CertificateException {
+            return;
+        }
+    }
+}
